d24fb366ce45926791e4fc041f6b11a37e81a0a9
Dean Wampler <dean@deanwampler.com>
2013-07-14 10:59:29 -0500

    Bug fix for test-hive.

--- test/hive/test.sh
+++ test/hive/test.sh
@@ -3,7 +3,8 @@
 # test/hive/test.sh - Test using Hive running in local mode.
 
 dir=$(dirname $0)
-echo $dir
+mkdir -p $dir/tmp/warehouse
+
 hive -f $dir/test.hql
 diff $dir/output/ngrams/000000_0 $dir/golden/ngrams/expected.txt
 test $? -ne 0 && exit 1
